490835a34180d57546cb39de8148454641c8fb86
Jaeyeonic <jyhuh35@gmail.com>
2019-05-29 22:17:06 +0900

    Revert "hjy"
    This reverts commit 4404c06ae40f93e6a7a6f9d144d2862d1b2d3313.

--- JaeYeonic/Code/1st/2016.py
+++
@@ -1,4 +0,0 @@
-import datetime
-def solution(a, b):
-    answer = ["MON","TUE","WED","THU","FRI","SAT","SUN"];
-    return answer[datetime.date(2016,a,b).weekday()]
--- JaeYeonic/Code/1st/marathon.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    answer = ''
-    for i in range(len(participant)):
-        for j in range(len(completion)):
-            if completion[j] == participant[i]:
-                del completion[j]
-            else:
-                answer += participant[i]
-    return answer
